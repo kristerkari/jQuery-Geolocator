6ae336edba26da9b484fc6978e5afb94a9328e03
Krister Kari <krister.kari@webtehdas.fi>
2012-08-19 01:29:55 +0300

    Added Grunt

---
+++ grunt.js
@@ -0,0 +1,67 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-sizediff');
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    lint: {
+      files: ['grunt.js', 'src/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: false,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    },
+    uglify: {},
+    sizediff: {
+      dist: {
+        files: [
+            'dist/<%= pkg.name %>.js',
+            'dist/<%= pkg.name %>.min.js'
+        ]
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('build', 'lint concat min sizediff');
+
+};
